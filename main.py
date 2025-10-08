ca8419b995904f1f23c7de3f2f96109e4c4a7702
Sambath Reasmey <104728907+sambathreasmey@users.noreply.github.com>
2025-10-08 13:33:49 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from flask_jwt_extended import get_jwt, get_jwt_identity, verify_jwt_in_request
 from config import Config
 from extensions import db, jwt
 from models import User
+from jinja2 import FileSystemBytecodeCache
 
 def create_app():
     app = Flask(__name__, static_folder="static", template_folder="templates")
+    app.jinja_env.bytecode_cache = FileSystemBytecodeCache('/tmp/jinja_cache', '%s.cache')
     app.config.from_object(Config)
 
     db.init_app(app)
